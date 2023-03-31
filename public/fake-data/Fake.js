6f017ee88c43541c468149160bf3e205da6a6726
golam-kibriya123 <golamkibriya622@gmail.com>
2023-03-31 05:45:53 -0500

    create invironment

---
+++ public/fake-data/Fake.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fake = () => {
+    return (
+        <div>
+            <img src="../img/depositphotos_330369456-stock-photo-smart-middle-aged-grayed-man.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Fake;
